9a959296a4bfc814f5ff93703ba9a09208401fa8
nicholas tang <tangkwonghoong@gmail.com>
2023-12-03 17:21:39 +0800

    edit rds dashboard and add friends-capstone-summary-ECS-Card CRUD APIs-CPUUtilization

--- cloudwatch-metric-alarm/friends-capstone-rds-dashboard.tf
+++ cloudwatch-metric-alarm/friends-capstone-rds-dashboard.tf
@@ -1,6 +1,6 @@
-resource "aws_cloudwatch_metric_alarm" "friends_capstone_CPUUtilization" {
-  alarm_name          = "friends-capstone-CPUUtilization"
-  alarm_description   = "This alarm helps to monitor consistent high CPU utilization. CPU utilization measures non-idle time. Consider using [Enhanced Monitoring](https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Monitoring.OS.Enabling.html) or [Performance Insights](https://aws.amazon.com/rds/performance-insights/) to review which [wait time](https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Monitoring-Available-OS-Metrics.html) is consuming the most of the CPU time (`guest`, `irq`, `wait`, `nice`, etc) for MariaDB, MySQL, Oracle, and PostgreSQL. Then evaluate which queries consume the highest amount of CPU. If you cannot tune your workload, consider moving to a larger DB instance class."
+resource "aws_cloudwatch_metric_alarm" "friends_capstone_summary_CPUUtilization" {
+  alarm_name          = "friends-capstone-summary-CPUUtilization"
+  alarm_description   = "This alarm helps you detect a high CPU utilization of the ECS service. If there is no ongoing ECS deployment, a maxed-out CPU utilization might indicate a resource bottleneck or application performance problems. To troubleshoot, you can increase the CPU limit."
   actions_enabled     = true
 
   alarm_actions       = [
@@ -8,46 +8,19 @@ resource "aws_cloudwatch_metric_alarm" "friends_capstone_CPUUtilization" {
     "arn:aws:sns:us-west-2:255945442255:Default_CloudWatch_Alarms_Topic"
   ]
 
-  metric_name         = "friends-capstone-CPUUtilization"
-  namespace           = "AWS/RDS"
+  metric_name         = "friends-capstone-summary-CPUUtilization"
+  namespace           = "AWS/ECS"
   statistic           = "Average"
 
   dimensions = {
-    Name  = "DBInstanceIdentifier"
-    Value = "friendscapstonerds"
+    ServiceName = "friends-capstone-crud-api-service"
+    ClusterName = "friends-capstone-cluster"
   }
 
-  period              = 60
+  period              = 300
   evaluation_periods  = 5
   datapoints_to_alarm = 5
-  threshold           = 90
+  threshold           = 100
   comparison_operator = "GreaterThanThreshold"
-  treat_missing_data  = "breaching"
-}
-
-resource "aws_cloudwatch_metric_alarm" "friends_capstone_FreeStorageSpace" {
-  alarm_name          = "friends-capstone-FreeStorageSpace"
-  alarm_description   = "friends-capstone-FreeStorageSpace. This alarm watches for a low amount of available storage space. Consider scaling up your database storage if you frequently approach storage capacity limits. Include some buffer to accommodate unforeseen increases in demand from your applications. Alternatively, consider enabling RDS storage auto scaling. Additionally, consider freeing up more space by deleting unused or outdated data and logs. For further information, check [RDS run out of storage document](https://repost.aws/knowledge-center/rds-out-of-storage) and [PostgreSQL storage issues document](https://repost.aws/knowledge-center/diskfull-error-rds-postgresql)."
-  actions_enabled     = true
-
-  alarm_actions       = [
-    "arn:aws:sns:us-west-2:255945442255:friends-capstone-alarm-slack",
-    "arn:aws:sns:us-west-2:255945442255:Default_CloudWatch_Alarms_Topic"
-  ]
-
-  metric_name         = "friends-capstone-FreeStorageSpace"
-  namespace           = "AWS/RDS"
-  statistic           = "Average"
-
-  dimensions = {
-    Name  = "DBInstanceIdentifier"
-    Value = "friendscapstonerds"
-  }
-
-  period              = 60
-  evaluation_periods  = 5
-  datapoints_to_alarm = 5
-  threshold           = 10
-  comparison_operator = "LessThanThreshold"
   treat_missing_data  = "breaching"
 }
--- cloudwatch-metric-alarm/friends-capstone-summary-dashboard.tf
+++ cloudwatch-metric-alarm/friends-capstone-summary-dashboard.tf
@@ -1,47 +1,83 @@
-resource "aws_cloudwatch_metric_alarm" "cloudwatch_alarm" {
-  # Intent            : "This alarm is used to detect high CPU utilization for the ECS service. Consistent high CPU utilization can indicate a resource bottleneck or application performance problems."
-  # Threshold Justification : "The service metrics for CPU utilization might exceed 100% utilization. However, we recommend that you monitor the metric for high CPU utilization to avoid impacting other services. Set the threshold to about 90-95%. We recommend that you update your task definitions to reflect actual usage to prevent future issues with other services."
-
-  alarm_name          = "AWS/ECS CPUUtilization ServiceName=friends-capstone-crud-api-service ClusterName=friends-capstone-cluster"
+resource "aws_cloudwatch_metric_alarm" "friends_capstone_summary_ECS_CPUUtilization" {
+  alarm_name          = "friends-capstone-summary-ECS-Card CRUD APIs-CPUUtilization"
   alarm_description   = "This alarm helps you detect a high CPU utilization of the ECS service. If there is no ongoing ECS deployment, a maxed-out CPU utilization might indicate a resource bottleneck or application performance problems. To troubleshoot, you can increase the CPU limit."
-  actions_enabled     = false
-  ok_actions          = []
-  alarm_actions       = []
-  insufficient_data_actions = []
-  metric_name         = "CPUUtilization"
+  actions_enabled     = true
+
+  alarm_actions       = [
+    "arn:aws:sns:us-west-2:255945442255:friends-capstone-alarm-slack",
+    "arn:aws:sns:us-west-2:255945442255:Default_CloudWatch_Alarms_Topic"
+  ]
+
+  metric_name         = "friends-capstone-summary-ECS-Card CRUD APIs-CPUUtilization"
   namespace           = "AWS/ECS"
   statistic           = "Average"
-  period              = 60
+
   dimensions = {
     ServiceName = "friends-capstone-crud-api-service"
     ClusterName = "friends-capstone-cluster"
-  }                  
+  }
+
+  period              = 300
   evaluation_periods  = 5
   datapoints_to_alarm = 5
   threshold           = 90
   comparison_operator = "GreaterThanThreshold"
-  treat_missing_data  = "breaching"
+  treat_missing_data  = "notBreaching"
 }
 
-resource "aws_cloudwatch_metric_alarm" "ecs_memory_alarm" {
-    #Intent : "This alarm monitors the Memory Utilization of an Amazon ECS (Elastic Container Service) cluster. Memory Utilization is a crucial metric for assessing the memory usage of your containerized applications. High Memory Utilization may indicate potential performance issues or resource constraints within your ECS tasks or services."
-    
-  alarm_name          = "ECS MemoryUtilization"
-  alarm_description   = "This alarm monitors the Memory Utilization of an Amazon ECS (Elastic Container Service) cluster. Memory Utilization is a crucial metric for assessing the memory usage of your containerized applications. High Memory Utilization may indicate potential performance issues or resource constraints within your ECS tasks or services."
+resource "aws_cloudwatch_metric_alarm" "friends_capstone_summary_ECS_MemoryUtilization" {
+  alarm_name          = "friends-capstone-summary-ECS-Card CRUD APIs-MemoryUtilization"
+  alarm_description   = "This alarm is triggered when the memory utilization of the system falls below a predefined threshold."
   actions_enabled     = true
-  alarm_actions       = ["arn:aws:sns:us-west-2:255945442255:Default_CloudWatch_Alarms_Topic"]
-  metric_name         = "MemoryUtilization"
-  namespace           = "AWS/ECS"
+
+  alarm_actions       = [
+    "arn:aws:sns:us-west-2:255945442255:friends-capstone-alarm-slack",
+    "arn:aws:sns:us-west-2:255945442255:Default_CloudWatch_Alarms_Topic"
+  ]
+
+  metric_name         = "friends-capstone-summary-ECS-Card CRUD APIs-MemoryUtilization"
+  namespace           = "."
+  statistic           = "Average"
+
+  dimensions = [
+    {
+      name  = "."
+      value = "."
+    }
+  ]
+
+  period              = 21600
+  evaluation_periods  = 1
+  datapoints_to_alarm = 1
+  threshold           = 10
+  comparison_operator = "LessThanThreshold"
+  treat_missing_data  = "notBreaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "friends_capstone_summary_RDS_CPUUtilization" {
+  alarm_name          = "friends-capstone-summary-RDS-CPUUtilization"
+  alarm_description   = "This alarm helps to monitor consistent high CPU utilization. CPU utilization measures non-idle time. Consider using [Enhanced Monitoring](https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Monitoring.OS.Enabling.html) or [Performance Insights](https://aws.amazon.com/rds/performance-insights/) to review which [wait time](https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Monitoring-Available-OS-Metrics.html) is consuming the most of the CPU time (`guest`, `irq`, `wait`, `nice`, etc) for MariaDB, MySQL, Oracle, and PostgreSQL. Then evaluate which queries consume the highest amount of CPU. If you cannot tune your workload, consider moving to a larger DB instance class."
+  actions_enabled     = true
+
+  alarm_actions       = [
+    "arn:aws:sns:us-west-2:255945442255:Default_CloudWatch_Alarms_Topic",
+    "arn:aws:sns:us-west-2:255945442255:friends-capstone-alarm-slack"
+  ]
+
+  metric_name         = "friends-capstone-summary-RDS-CPUUtilization"
+  namespace           = "AWS/RDS"
   statistic           = "Average"
 
   dimensions = {
-    # No specific dimensions for ECS MemoryUtilization in this case
+    Name  = "DBInstanceIdentifier"
+    Value = "friendscapstonerds"
   }
 
-  period             = 21600
-  evaluation_periods = 1
-  datapoints_to_alarm = 1
-  threshold          = 0.1
+  period              = 300
+  evaluation_periods  = 5
+  datapoints_to_alarm = 5
+  threshold           = 90
   comparison_operator = "GreaterThanThreshold"
-  treat_missing_data = "missing"
+  treat_missing_data  = "notBreaching"
 }
+
