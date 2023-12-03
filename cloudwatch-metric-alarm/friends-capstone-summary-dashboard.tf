fbae46e1cfc2c3a208d4041320c23ef16cc106aa
nicholas tang <tangkwonghoong@gmail.com>
2023-12-03 17:24:47 +0800

    add alarm friends-capstone-summary-RDS-CPUUtilization

--- cloudwatch-metric-alarm/friends-capstone-summary-dashboard.tf
+++ cloudwatch-metric-alarm/friends-capstone-summary-dashboard.tf
@@ -53,3 +53,30 @@ resource "aws_cloudwatch_metric_alarm" "friends_capstone_summary_ECS_MemoryUtili
   comparison_operator = "LessThanThreshold"
   treat_missing_data  = "notBreaching"
 }
+
+resource "aws_cloudwatch_metric_alarm" "friends_capstone_summary_RDS_CPUUtilization" {
+  alarm_name          = "friends-capstone-summary-RDS-CPUUtilization"
+  alarm_description   = "This alarm helps to monitor consistent high CPU utilization. CPU utilization measures non-idle time. Consider using [Enhanced Monitoring](https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Monitoring.OS.Enabling.html) or [Performance Insights](https://aws.amazon.com/rds/performance-insights/) to review which [wait time](https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Monitoring-Available-OS-Metrics.html) is consuming the most of the CPU time (`guest`, `irq`, `wait`, `nice`, etc) for MariaDB, MySQL, Oracle, and PostgreSQL. Then evaluate which queries consume the highest amount of CPU. If you cannot tune your workload, consider moving to a larger DB instance class."
+  actions_enabled     = true
+
+  alarm_actions       = [
+    "arn:aws:sns:us-west-2:255945442255:Default_CloudWatch_Alarms_Topic",
+    "arn:aws:sns:us-west-2:255945442255:friends-capstone-alarm-slack"
+  ]
+
+  metric_name         = "friends-capstone-summary-RDS-CPUUtilization"
+  namespace           = "AWS/RDS"
+  statistic           = "Average"
+
+  dimensions = {
+    Name  = "DBInstanceIdentifier"
+    Value = "friendscapstonerds"
+  }
+
+  period              = 300
+  evaluation_periods  = 5
+  datapoints_to_alarm = 5
+  threshold           = 90
+  comparison_operator = "GreaterThanThreshold"
+  treat_missing_data  = "notBreaching"
+}
