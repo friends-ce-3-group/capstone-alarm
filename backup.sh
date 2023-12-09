9ff27b1f7b02edaed2b756b68fd8034e0245f9e5
nekolas <tangkwonghoong@gmail.com>
2023-12-09 13:03:44 +0800

    modify command

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-aws cloudwatch describe-alarms --query 'MetricAlarms[?contains(AlarmName, 'friends')]' --region us-west-2 > ./alarms_backup/alarms_backup.json
+aws cloudwatch describe-alarms --query 'MetricAlarms[?contains(AlarmName, 'friends')]' --region "us-west-2" > ./alarms_backup/alarms_backup.json
 cat ./alarms_backup/alarms_backup.json 
