3c4f227392887553155676f68b96e71b6bc343f6
nekolas <tangkwonghoong@gmail.com>
2023-12-09 13:02:11 +0800

    show contents

--- backup.sh
+++ backup.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 aws cloudwatch describe-alarms --query 'MetricAlarms[?contains(AlarmName, 'friends')]' --region us-west-2 > ./alarms_backup/alarms_backup.json
+cat ./alarms_backup/alarms_backup.json 
